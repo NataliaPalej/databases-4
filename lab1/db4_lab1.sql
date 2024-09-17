386723956547422bc9434f0226336be4950300f1
Natalia <PalejNatalia@gmail.com>
2024-09-17 14:56:54 +0100

    Lab2 Answers

--- lab1/db4_lab1.sql
+++ lab1/db4_lab1.sql
@@ -1,3 +1,6 @@
+------------------------------------------------------------------------------------------------
+# # # # LECTURE 1 # # # #
+------------------------------------------------------------------------------------------------
 show databases;
 
 drop database if exists db4_lab1;
@@ -61,3 +64,57 @@ select director, city, sum(assets) as total_assets from branch group by director
 select director, city, sum(assets) as total_assets from branch group by director, city with rollup;
 
 select * from branch;
+
+------------------------------------------------------------------------------------------------
+# # # # LECTURE 2 # # # #
+------------------------------------------------------------------------------------------------
+# insert values into a table, "assets" column will default to NULL
+INSERT INTO branch (name, city, director) VALUES ("branch 10", "Dublin", "Joe Maxi");
+
+# test for IS NULL output 
+select name, city, director from branch where assets IS NULL;
+select name, city, director from branch where assets IS NOT NULL;
+
+select * from branch;
+
+# Update 
+UPDATE branch SET assets=600 WHERE name LIKE "branch 1";
+
+# Delete
+DELETE from branch WHERE city="Roscommon";
+
+# Deletes Everything
+DELETE from branch;
+
+SELECT name, city, assets, NOW() as assets_date FROM branch;
+
+# AND condition
+SELECT * FROM branch WHERE city LIKE "Athlone" AND director LIKE "J.Bloggs";
+# OR condition
+SELECT * FROM branch WHERE city LIKE "Athlone" OR director LIKE "J.Public";
+
+------------------------------------------------------------------------------------------------
+# # # # # # SUB-QUERIES # # # # # #
+------------------------------------------------------------------------------------------------
+create table branch_list (name varchar(10));
+insert into branch_list values ("Branch 1");
+insert into branch_list values ("Branch 2");
+insert into branch_list values ("Branch 3");
+
+select * from branch_list;
+
+# return columns from branch table where name matches branch_list table
+select distinct name, city, director from branch where name in (select name from branch_list);
+
+
+------------------------------------------------------------------------------------------------
+# # # # # # JOINS # # # # # #
+------------------------------------------------------------------------------------------------
+create table Persons;
+create table Persons (name varchar(10), city varchar(20), director varchar(20), assets integer);
+create table Orders (name varchar(10), city varchar(20), director varchar(20), assets integer);
+
+SELECT Persons.LastName, Persons.FirstName, Orders.OrderNo FROM Persons INNER JOIN Orders ON Persons.P_Id=Orders.P_Id ORDER BY Persons.LastName;
+SELECT Persons.LastName, Persons.FirstName, Orders.OrderNo FROM Persons LEFT JOIN Orders ON Persons.P_Id=Orders.P_Id ORDER BY Persons.LastName;
+SELECT Persons.LastName, Persons.FirstName, Orders.OrderNo FROM Persons RIGHT JOIN Orders ON Persons.P_Id=Orders.P_Id ORDER BY Persons.LastName;
+SELECT Persons.LastName, Persons.FirstName, Orders.OrderNo FROM Persons FULL JOIN Orders ON Persons.P_Id=Orders.P_Id ORDER BY Persons.LastName;
---
+++ lab2/Lab1_SQL_lab_details.sql
@@ -0,0 +1,107 @@
+DROP DATABASE IF EXISTS DB4_2024;
+CREATE DATABASE IF NOT EXISTS DB4_2024;
+USE DB4_2024;
+
+DROP TABLE IF EXISTS details;
+
+
+CREATE TABLE DETAILS (
+	id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	firstName VARCHAR(15) NOT NULL,
+	lastName VARCHAR(20) NOT NULL,
+	age INTEGER NOT NULL,
+	gender VARCHAR(1) NOT NULL,
+	position VARCHAR(15) NOT NULL,
+	department VARCHAR(20) NOT NULL,
+	rate DECIMAL(7, 2) NOT NULL,
+	hours INTEGER NOT NULL);
+
+SELECT 'INSERTING DATA INTO DATABASE' as 'INFO';
+
+INSERT INTO details VALUES ( null, 'Joe', 'Mullins', 64, 'M', 'Lecturer', 'Engineering', 63.08, 12);
+INSERT INTO details VALUES ( null, 'Joan', 'Macgill', 27, 'F', 'Researcher', 'Science', 38, 35);
+INSERT INTO details VALUES ( null, 'Jim', 'Mitchell', 51, 'M', 'Researcher', 'Business', 38, 25);
+INSERT INTO details VALUES ( null, 'John', 'Magner', 47, 'M', 'Lecturer', 'Humanities', 63.08, 16);
+INSERT INTO details VALUES ( null, 'Jean', 'Madden', 45, 'F', 'Professor', 'Design', 76.45, 14);
+INSERT INTO details VALUES ( null, 'Jack', 'Minogue', 61, 'M', 'Administrator', 'Hospitality', 45.57, 37);
+INSERT INTO details VALUES ( null, 'Josephine', 'Mahony', 33, 'F', 'Head', 'Nursing', 98.56, 40);
+INSERT INTO details VALUES ( null, 'Juan', 'Mosley', 56, 'M', 'Professor', 'Engineering', 76.45, 11);
+INSERT INTO details VALUES ( null, 'Jamie', 'Mulllen', 45, 'M', 'Researcher', 'Science', 38, 37);
+INSERT INTO details VALUES ( null, 'Julie', 'Mooney', 39, 'F', 'Lecturer', 'Business', 63.08, 18);
+INSERT INTO details VALUES ( null, 'Jane', 'Mccarthy', 37, 'F', 'Administrator', 'Design', 45.57, 45);
+INSERT INTO details VALUES ( null, 'James', 'May', 38, 'M', 'Researcher', 'Hospitality', 38, 9);
+INSERT INTO details VALUES ( null, 'Joseph', 'Manning', 32, 'M', 'Lecturer', 'Hospitality', 63.08, 16);
+INSERT INTO details VALUES ( null, 'Judith', 'Milner', 36, 'F', 'Lecturer', 'Nursing', 63.08, 20);
+INSERT INTO details VALUES ( null, 'Jerome', 'Murphy', 26, 'M', 'Head', 'Engineering', 98.56, 42);
+INSERT INTO details VALUES ( null, 'Jude', 'Manley', 28, 'M', 'Head', 'Science', 98.56, 41);
+INSERT INTO details VALUES ( null, 'Juanita', 'Mahon', 59, 'F', 'Administrator', 'Engineering', 45.57, 49);
+INSERT INTO details VALUES ( null, 'Justin', 'Maguire', 25, 'M', 'Lecturer', 'Business', 63.08, 16);
+INSERT INTO details VALUES ( null, 'Jacqulinen', 'Musgrave', 43, 'F', 'Professor', 'Business', 76.45, 10);
+INSERT INTO details VALUES ( null, 'Julia', 'Moore', 36, 'F', 'Administrator', 'Science', 45.57, 38);
+
+SELECT * from details;
+
+#1.	Get the details of all employees who are not between the ages of 40 and 50.
+select * from details where age < 40 and age < 50;
+
+#2.	Get the details of all employees who work between the 10 and 15 hours per week.
+select * from details where hours >= 10 and hours <= 15;
+
+#3.	Find all employees who have an ‘e’ in their first name.
+select * from details where firstName like "%e%";
+
+#4.	Find all employees whose second letter of their first name is ‘u’
+select * from details where firstName like "_u%";
+
+#5.	Find all employees whose first name:
+#	- ends with n and is only 4 letters in length
+select * from details where firstName like "___n";
+
+#	- starts with a ‘J’ but is only 4 letters in length
+select * from details where firstName like "J___";
+
+#	- is only 3 letters in length
+select * from details where length(firstName) = 3;
+
+#	- Is greater than 4 letters in length
+select * from details where length(firstName) > 4;
+
+#6.	Find all female employees who have a ‘3’ in their age.
+select * from details where gender="F" and age like "%3%";
+
+#7.	Count the number of females.
+select count(*) as totalFemales from details where gender="F";
+
+#8.	Count the number of males.
+select count(*) as totalMales from details where gender="M";
+
+#9.	Calculate the average age of the females.
+select round(avg(age), 2) as avgFemaleAge from details where gender="F";
+
+#10.	Calculate the average age of the males.
+select round(avg(age), 2) as avgMaleAge from details where gender="M";
+
+#11.	Who is the oldest employee?
+select firstName, lastName, max(age) as OldestEmployee from details;
+
+#12.	Who is the youngest employee?
+select firstName, lastName, min(age) as YoungestEmployee from details;
+
+#13.	What is the average hours worked?
+select round(avg(hours), 2) as avgHours from details;
+
+#14.	What is the total of the female wage?
+select round(sum(rate), 2) as totalFemaleWage from details where gender = "F";
+
+#15.	What is the total of the male wage?
+select round(sum(rate), 2) as totalMaleWage from details where gender = "M";
+
+#16.	What is the average age in each department?
+select department, round(avg(age)) as avgDeptAge from details group by department;
+
+#17.	What is the average age of each position?
+select position, round(avg(age)) as avgPositionAge from details group by position;
+
+#18.	Count the number of males/females per department.  ????????
+select count(*) as totalDeptMales from details where gender="M";
+
