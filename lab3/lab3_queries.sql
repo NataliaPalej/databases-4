556ff8c686cc5edbd506e34622965759c2c7d10f
Natalia <PalejNatalia@gmail.com>
2024-09-18 10:30:20 +0100

    Updated Databases.one notes with Lab3, uploaded lab3 queries

---
+++ lab3/lab3_queries.sql
@@ -0,0 +1,15 @@
+select * from actor CROSS JOIN address 
+UNION 
+select * from actor cross join address 
+UNION 
+select * from actor cross join address 
+UNION 
+select * from actor cross join address;
+
+show full processlist;
+
+# Explain query returns information about table
+select * from actor;
+explain select * from actor;
+explain extended select * from actor cross join address;
+explain select * from actor inner join film_actor on actor.actor_id = film_actor.actor_id;
